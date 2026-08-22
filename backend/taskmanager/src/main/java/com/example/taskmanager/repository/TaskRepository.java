26c0fe8a04cb64b7ab6b401c815e276ed3bd9426
kWRizzz <krishnathey2j@gmail.com>
2026-08-22 23:56:50 +0530

    temp frontend fix aded cors at backend

--- backend/taskmanager/src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -3,5 +3,7 @@ package com.example.taskmanager.repository;
 import com.example.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface TaskRepository extends JpaRepository<Task,Long> {
 }
