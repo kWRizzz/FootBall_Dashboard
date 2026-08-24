9a59e7916b5cd4ee81a2f292386881c98ae3b864
kWRizzz <krishnathey2j@gmail.com>
2026-08-24 22:38:48 +0530

    DTO implemented

--- backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskRequest.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskRequest.java
@@ -1,4 +1,32 @@
 package com.example.taskmanager.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class TaskRequest {
+
+    @NotBlank(message = "ENter some input")
+    @Size(min = 3,max = 6,message = "Range of size 3 to 6")
+
+    private String title;
+    private boolean completed;
+
+    public TaskRequest(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
 }
