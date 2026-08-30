0554d25df1832361dd686f31dc86cbc3722bc92d
kWRizzz <krishnathey2j@gmail.com>
2026-08-30 20:53:51 +0530

    DTO Updating some errorResponse according to dto

---
+++ backend/taskmanager/src/main/java/com/example/taskmanager/dto/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.example.taskmanager.dto;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+
+    private ErrorResponse(int status, String message){
+        this.status=status;
+        this.message=message;
+        this.timestamp= LocalDateTime.now();
+    }
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
--- backend/taskmanager/src/main/java/com/example/taskmanager/service/TaskServices.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/service/TaskServices.java
@@ -70,6 +70,4 @@ public class TaskServices {
         taskRepository.delete(task);
     }
 
-
-    
 }
