69cf05f9434b5fba643a6af7159fadc833a918e9
kWRizzz <krishnathey2j@gmail.com>
2026-08-31 12:09:31 +0530

    updated user model

--- backend/taskmanager/src/main/java/com/example/taskmanager/model/User.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/model/User.java
@@ -2,6 +2,8 @@ package com.example.taskmanager.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name="user")
 public class User {
@@ -13,6 +15,9 @@ public class User {
     private String name;
     private String email;
     private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
 
     public User(){}
 
