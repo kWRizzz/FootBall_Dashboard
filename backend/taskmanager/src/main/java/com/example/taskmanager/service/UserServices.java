74f1d2662d3caa8580deb28f7254b154b19987ec
kWRizzz <krishnathey2j@gmail.com>
2026-08-31 11:45:09 +0530

    User services made

--- backend/taskmanager/src/main/java/com/example/taskmanager/service/UserServices.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/service/UserServices.java
@@ -1,4 +1,26 @@
 package com.example.taskmanager.service;
 
+import com.example.taskmanager.model.User;
+import com.example.taskmanager.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserServices {
+
+    private UserRepository userRepository;
+
+    public UserServices (UserRepository userRepository) {
+        this.userRepository=userRepository;
+    }
+
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUser(User user){
+        return userRepository.findAll();
+    }
 }
