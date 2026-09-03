381846b1be2321a1f1cd176ca080c3834a9fad86
kWRizzz <krishnathey2j@gmail.com>
2026-09-03 16:23:34 +0530

    implemented customUserDetailed section In services moving to make the services of it

--- backend/taskmanager/src/main/java/com/example/taskmanager/service/CustomUserDetails.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/service/CustomUserDetails.java
@@ -1,4 +1,51 @@
 package com.example.taskmanager.service;
 
-public class CustomUserDetails {
+import com.example.taskmanager.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user){
+        this.user= user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername(){
+        return user.getName();
+    }
+
+//    @Override boolean isAccountNonExpired(){
+//        return true;
+//    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
 }
