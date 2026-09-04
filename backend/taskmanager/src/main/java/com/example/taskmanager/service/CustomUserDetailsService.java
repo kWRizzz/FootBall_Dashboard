ed0b6c6968dee96a8548c540cfdc01c713f79086
kWRizzz <krishnathey2j@gmail.com>
2026-09-04 23:06:40 +0530

    something something

--- backend/taskmanager/src/main/java/com/example/taskmanager/service/CustomUserDetailsService.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/service/CustomUserDetailsService.java
@@ -1,4 +1,31 @@
 package com.example.taskmanager.service;
 
-public class CustomUserDetailsService {
+import com.example.taskmanager.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements CustomUserDetails{
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUserName(String username)
+        throws UsernameNotFoundException {
+        User user =userRepository.findByUsername(username)
+                .orElseThrow(()->
+                            new UsernameNotFoundException(
+                                    "user not found" + username
+                            )
+                        );
+
+        return new CustomUserDetails(user);
+    }
 }
